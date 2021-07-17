cb0d3f501da9a138d2e6728cabfe3836486566c6
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-07-17 15:15:31 -0300

    created hook for use-brands

---
+++ src/hooks/use-brands.js
@@ -0,0 +1,16 @@
+import React from "react";
+import getBrandsService from "../services/get-brands-service";
+
+function useBrands() {
+    const [brands, setBrands] = React.useState([]);
+
+    React.useEffect(() => {
+        getBrandsService().then((data) => {
+            setBrands(data)
+        });
+    });
+
+    return {brands};
+}
+
+export default useBrands;
